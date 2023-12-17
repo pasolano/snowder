d521149cc9cf3f840781d271c65a42da569f6adc
Pablo Solano <pasolano@email.wm.edu>
2023-12-16 23:24:28 -0800

    no slidey slidey

--- player.gd
+++ player.gd
@@ -27,8 +27,12 @@ func _physics_process(delta):
 			var direction = Vector2.ZERO
 			if Input.is_action_pressed("move_right"):
 				velocity.x += speed * delta
+			else:
+				velocity.x = clampf(velocity.x, -INF, 0)
 			if Input.is_action_pressed("move_left"):
 				velocity.x -= speed	* delta
+			else:
+				velocity.x = clampf(velocity.x, 0, INF)
 			if Input.is_action_pressed("move_up") and is_on_floor():
 				velocity.y = jump_speed
 			
