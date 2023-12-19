532ef3b691f6dbd463c9a9761dbe5de152c2da10
Pablo Solano <pasolano@email.wm.edu>
2023-12-19 01:42:55 -0800

    spawnpoint find player by group

--- SpawnPoint.gd
+++ SpawnPoint.gd
@@ -1,16 +1,13 @@
 extends Marker2D
 
-@export var spawnee: CollisionObject2D
-
 # relative position to walk to
-@export var walk_to: Vector2 = Vector2.ZERO
+# @export var walk_to := Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	assert(spawnee)
-	spawnee.position = position
+	var player := get_tree().get_first_node_in_group("Player")
+	player.position = position
 
 #func _physics_process(delta):
 	#if spawnee.position != self.position + walk_to: # how to check if they go beyond
 		#pass
-		## 
